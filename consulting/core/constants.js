ac1fcb73ff56e024b822e6b0f9d846d0b47f95b6
Goseb10 <128860328+Goseb10@users.noreply.github.com>
2025-10-25 17:15:24 +0200

    constants.js

--- consulting/core/constants.js
+++ consulting/core/constants.js
@@ -5,6 +5,9 @@ export const AGE_TAXE = 60;
 export const AGE_FINALE_DEFAUT = 67;
 export const AGE_FINALE_LT = 80;
 
+// NOUVELLE CONSTANTE AJOUTÉE
+export const AGE_MAX_VERSEMENT_EP = 64; // On ne verse plus dans une EP après 64 ans
+
 export const FRAIS_DEFAUT = {
     pension: { fraisEntree: 3, fraisCourant: 0.85 },
     'long-terme': { fraisEntree: 3, fraisCourant: 1 }
